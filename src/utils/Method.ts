f222d51083f397c263dbfa75dcfd02a5d646fa0a
midori mici <midorimici.works@gmail.com>
2020-07-30 23:37:18 +0900

    add methods & fix: getting ()

--- src/utils/Method.ts
+++ src/utils/Method.ts
@@ -1,29 +1,115 @@
+import makeQuerySelector from './Selector';
+
 export default (name: string, args: string): string => {
-	//// filtering ////
-	// .first()
-	if (name === 'first') return '_[0]';
+	switch (name) {
+		//// filtering ////
+		// .first()
+		case 'first': return '%_%[0]';
+		
+		// .last()
+		case 'last': return 'Array.from(%_%).slice(-1)[0]';
+		
+		// .eq()
+		case 'eq': return `Array.from(%_%).slice(${args})[0]`;
+
+		// .slice()
+		case 'slice': return `Array.from(%_%).slice(${args})`;
+
+		// .even()
+		case 'even': return `Array.from(%_%).filter((e, i) => i % 2 === 0)`;
+
+		// .odd()
+		case 'odd': return `Array.from(%_%).filter((e, i) => i % 2 === 1)`;
+
+		// .filter()
+		case 'filter': {
+			/*
+			let regex: RegExp = /(?:function[\s\n]*\([\s\n]*(\w+)?[\s\n]*,?[\s\n]*(\w+)?[\s\n]*\)|\([\s\n]*(\w+)[\s\n]*,[\s\n]*(\w+)[\s\n]*\)[\s\n]*=>)[\s\n]*{?((?:.|\n)*?)}?\)/g;
+			let match: string[] = [...args.matchAll(regex)][0];
+			if (match) {
+				if (match[1]) {
+					if (match[5]) {
+						return `Array.from(%_%).filter(function (${match[2] || '_'}, ${match[1]}) {${match[6]}})`
+					} else {
+						return `Array.from(%_%).filter(function (${match[2] || '_'}, ${match[1]}) ${match[6]})`
+					}
+				} else if (match[3]) {
+					if (match[5]) {
+						return `Array.from(%_%).filter((${match[4] || '_'}, ${match[3]}) => {${match[6]}})`
+					} else {
+						return `Array.from(%_%).filter((${match[4] || '_'}, ${match[3]}) => ${match[6]})`
+					}
+				}
+			}
+			*/
+
+			let regex: RegExp = /(?:\$|jQuery)\((.*["'`] *)\)/g;
+			let match: string[] = [...args.matchAll(regex)][0];
+			if (match) {
+				return `%_%.map(e => ${makeQuerySelector(match[1])})`;
+			}
+
+			return `%_%.map(e => ${makeQuerySelector(args)})`;
+		}
+
+		// .has()
+		case 'has': return `Array.from(%_%).filter(e => e.children.indexOf(e.${makeQuerySelector(args)[0]})`;
+
+
+
+		//// tree traversal ////
+		// .find()
+		case 'find': return `%_%.map(e => e.${makeQuerySelector(args)})`;
+
+		// .children()
+		case 'children': return `%_%.map(e => querySelector(":scope > ${args || '*'}"))`;
+
+		// .parent()
+		case 'parent': return `%_%.map(e => parentNode)`;
+
+		// .closest()
+		case 'closest': return `%_%.map(e => closest(${args}))`;
+
+		// .next()
+		case 'next': return `%_%.map(e => nextElementSibling)`;
+
+		// .prev()
+		case 'prev': return `%_%.map(e => previousElementSibling)`;
+
+		// .siblings()
+		case 'siblings': return `%_%.map(e => parentNode.children)`;
+
+
 	
-	// .last()
-	if (name === 'last') return 'Array.from(_).slice(-1)[0]';
-	
-	// .eq()
-	if (name === 'eq') return `Array.from(_).slice(${args})[0]`;
+		//// attributes ////
+		// .addClass()
+		case 'addClass': return `%_%.forEach(e => e.classList.add(${args}))`;
 
-	// .slice()
-	if (name === 'slice') return `Array.from(_).slice(${args})`;
+		// .removeClass()
+		case 'removeClass': return `%_%.forEach(e => e.classList.remove(${args}))`;
 
-	// .even()
-	if (name === 'even') return `Array.from(_).filter((e, i) => i % 2 === 0)`;
+		// .toggleClass()
+		case 'toggleClass': return `%_%.forEach(e => e.classList.toggle(${args}))`;
 
-	// .odd()
-	if (name === 'odd') return `Array.from(_).filter((e, i) => i % 2 === 1)`;
+		// .hasClass()
+		case 'hasClass': return `%_%.some(e => e.classList.contains(${args}))`;
 
-	// .not()
-	if (name === 'not') {
-		let regex: RegExp = /function[\s\n]*\([\s\n]*(\w+)[\s\n]*,[\s\n]*(\w+)[\s\n]*\)[\s\n]*{[\s\n]*/g
+		// .attr()
+		case 'attr':
+			if (args.split(',').length === 1) return `%_%[0].getAttribute(${args})`;
+			if (args.split(',').length === 2) return `%_%.forEach(e => e.getAttribute(${args[0]}) = ${args[1]})`;
+			break;
+
+		// .removeAttr()
+		case 'removeAttr': return `%_%.forEach(e => e.removeAttribute(${args}))`;
+
+		// .html()
+		case 'html': return args ? `%_%.forEach(e => e.innerHTML = ${args})` : `%_%[0].innerHTML`;
+
+		// .val()
+		case 'val': return args ? `%_%.forEach(e => e.value = ${args})` : `%_%[0].value`;
+
+
+		default: return '';
 	}
-
-
-
-	return ''
 };
--- src/utils/jqjsconv.ts
+++ src/utils/jqjsconv.ts
@@ -5,17 +5,24 @@ export default (t: string) => {
 	let rtn: string = '';
 
 	// selector
-	let regex: RegExp = /(?:\$|jQuery)\((.*["'`] *)\)/g;
-	if (t.match(regex)) {
-		rtn += `document.${makeQuerySelector(t.replace(regex, '$1'))}`;
+	// with following method
+	let flag: number = 0;
+	for (let i = 0; i < t.length; ++i) {
+		if (t[i] === '(') ++flag;
+		if (t[i] === ')') {
+			--flag;
+			if (flag === 0) flag = i; break;
+		}
 	}
 
-	t = t.replace(regex, '');
+	rtn += `document.${makeQuerySelector(t.slice(0, flag + 1).replace(/(?:\$|jQuery)\((.*["'`] *)\)/g, '$1'))}`;
+
+	t = t.replace(t.slice(0, flag + 1), '');
 
 	// method
-	regex = /\.(\w+)\((.*)\)/g;
+	let regex: RegExp = /\.(\w+)\((.*)\)/g;
 	if (t.match(regex)) {
-		rtn = `${makeMethod(t.replace(regex, '$1'), t.replace(regex, '$2'))}`.replace('_', rtn);
+		rtn = `${makeMethod(t.replace(regex, '$1'), t.replace(regex, '$2'))}`.replace(/%_%/g, rtn);
 	}
 
 	return rtn;
