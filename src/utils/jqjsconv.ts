8ac5501a8fdbb86d1b844d14bed927915bece2be
midori mici <midorimici.works@gmail.com>
2020-08-04 21:14:10 +0900

    fix:  interpretation

--- src/utils/jqjsconv.ts
+++ src/utils/jqjsconv.ts
@@ -16,10 +16,11 @@ const parBlock = (str: string): string => {
 
 export default (t: string) => {
 	let rtn: string = '';
+	let sel: string = '';
 	let regex: RegExp = /./g;
 
-	const parse = (s: string): string => {
-		let ret: string = '';
+	const parse = (s: string, el?: string): string[] => {
+		let ret: string = el || '';
 		// selector
 		regex = /(?:\$|jQuery)\((.*["'`] *)\)/g;
 		if (s.match(regex)) {
@@ -31,6 +32,8 @@ export default (t: string) => {
 			ret = 'e';
 			s = s.slice(1);
 		}
+
+		let selector: string = ret;
 
 		// method
 		regex = /\.(\w+)\(((?:.|\s)*)\)/g;
@@ -44,23 +47,28 @@ export default (t: string) => {
 			}
 		}
 
-		return ret;
+		// single elements, no need to handle as an array
+		ret = ret.replace(/(?:Array\.from\()?([\w.("'`\-)]*getElementById\(["'`\w]+\)|.*\[0\]|e)(?:\[0\]|\).(?:map|forEach)\(e => e((?:.|\s)*)\))/g, `$1$2`);
+
+		return [ret, selector];
 	}
 
-	rtn = parse(t);
+	[rtn, sel] = parse(t);
 
 	// this
 	regex = /(?:\$|jQuery)\( *this *\)/g;
-	if (rtn.match(regex)) rtn = rtn.replace(regex, 'e');
+	let ematch: string[] | null = rtn.match(/forEach\(e =>|map\(e =>/g);
+	if (rtn.match(regex)) rtn = rtn.replace(regex, ematch ? 'e' : sel);
 	regex = /(?:\$|jQuery)\((.*), *this *\)/g;
-	if (rtn.match(regex)) rtn = rtn.replace(regex, `e.${makeQuerySelector([...rtn.matchAll(regex)][0][1])}`);
+	if (rtn.match(regex)) rtn = rtn.replace(regex,
+		`${rtn.match(/forEach\(e =>|map\(e =>/g) ? 'e' : sel}.${
+			makeQuerySelector([...rtn.matchAll(regex)][0][1])}`);
+	console.log(rtn)
 
 	// inside function
 	regex = /(?:function\s*\(\s*[^)]*\s*\)|\(\s*[^)]*\s*\)\s*=>)\s*{?\s*(.*)\s*}?/g;
 	let match: string[] = [...rtn.matchAll(regex)][0];
-	if (match) rtn = rtn.replace(match[1], parse(match[1]));
-
-	rtn = rtn.replace(/(?:Array\.from\()?([\w.("'`\-)]*getElementById\(["'`\w]+\)|.*\[0\]|e)(?:\[0\]|\).(?:map|forEach)\(e => e(.*)\))/g, `$1$2`);
+	if (match) rtn = rtn.replace(match[1], ematch ? parse(match[1])[0] : parse(match[1].slice(sel.length), sel)[0]);
 
 	rtn = (rtn || t).replace(/event\.page([XY])/g, 'MouseEvent.page$1');
 
